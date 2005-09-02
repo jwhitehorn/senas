0072c098cf6141a04c0f7e0f7d090f805023ec72
Jason Whitehorn <jason@whitehorn.us>
2005-09-02 01:44:25 +0000

    More ranker stuff

--- ranker.pl
+++ ranker.pl
@@ -38,86 +38,97 @@ while(<FILE>){
 }
 close FILE;
 use DBI;    #only works with transactional MySQL
+#!/usr/bin/perl
 use POSIX qw(setsid);
+use Fcntl;
+my $pipe = "/home/jason/foo.bar";
 
+if(lc($ARGV[0]) eq "stop"){
+        open FIFO, ">$pipe";
+        print FIFO "stop\n";
+        close FIFO;
+        exit 0;
+}
+if(!(lc($ARGV[0]) eq "start")){
+        print "Error, invalid argument!\n";
+        exit 1;
+}
+#else... start the daemon
 
 chdir("/");
 open STDIN, '/dev/null';
-#open STDOUT, '>/dev/null';
+open STDOUT, '>/dev/null';
 open STDERR, '>/dev/null';
 umask(0);
 my $pid = fork();
 exit if $pid;   #exit if we are the parent
 setsid or die $!;
+sysopen(FIFO, "$pipe", O_NONBLOCK|O_RDONLY) or die $!;
 
-#we are now a daemon... get to work!
-my $running = 1;	#go!
-sub Ranker{
-#	print "[DEBUG::Ranker] Ranker started.\n" unless !$debug;
-	$db = DBI->connect("DBI:mysql:$DB:$DBHost", "$DBUser", "$DBPassword")
-		or die "Error connecting to database\n";
-	while($running){
-		my $start = time();
-		my %rating = ();
-		my %temp = (); 
-		my %MD5s = ();
-		my @urls = ();
-		my $start = 0;
-		my $elements = 0;
-		my $converge = 0.001;
-		$query = "select URL, MD5 from `Sources`;";    #get EVERYTHING...this will take a while
-		$sth = $db->prepare($query);
-		$sth->execute();
-		$elements = $sth->rows;
-		my $URL;
-#		print "[DEBUG::Ranker] Beginning ranking $elements elements.\n" unless !$debug;
-		while($rows = $sth->fetchrow_arrayref()){
-			$URL = $rows->[0];
-			$MD5 = $rows->[1];
-			$rating{$URL} = 1.0;
-			$MD5s{$URL} = $MD5;
-			push @urls, $URL;
-		}
-		for($i = 0; $i != 10; $i++){
-		# print "$i";
-		#	print ".";
-			foreach (@urls){
-				$temp{$_} = $rating{$_};   #make a copy!
+my $command;
+		#	print "[DEBUG::Ranker] Ranker started.\n" unless !$debug;
+$db = DBI->connect("DBI:mysql:$DB:$DBHost", "$DBUser", "$DBPassword")
+				or die "Error connecting to database\n";
+while(1){
+        $command = <FIFO>;
+        if($command =~ m/stop/i){
+				$sth->finish;
+				$db->disconnect();
+                exit;   #got stop command!
+        }else{
+			my $start = time();
+			my %rating = ();
+			my %temp = (); 
+			my %MD5s = ();
+			my @urls = ();
+			my $start = 0;
+			my $elements = 0;
+			my $converge = 0.001;
+			$query = "select URL, MD5 from `Sources`;";    #get EVERYTHING...this will take a while
+			$sth = $db->prepare($query);
+			$sth->execute();
+			$elements = $sth->rows;
+			my $URL;
+			while($rows = $sth->fetchrow_arrayref()){
+				$URL = $rows->[0];
+				$MD5 = $rows->[1];
+				$rating{$URL} = 1.0;
+				$MD5s{$URL} = $MD5;
+				push @urls, $URL;
 			}
-			foreach (@urls){
-			#    print "|";
-				my $voter = $_;
-				$MD5 = $MD5s{$voter};
-				$MD5 = $db->quote($MD5);
-				$query = "select Target from Links where Source=$MD5;";
-				$sth = $db->prepare($query);
-				$sth->execute();
-				while(@row = $sth->fetchrow_array()){
-					if(!($row[0] eq $voter)){
-						$temp{$row[0]} += $rating{$voter} * $converge;
-						if(!$running){
-							return;
+			for($i = 0; $i != 10; $i++){
+				foreach (@urls){
+					$temp{$_} = $rating{$_};   #make a copy!
+				}
+				foreach (@urls){
+					my $voter = $_;
+					$MD5 = $MD5s{$voter};
+					$MD5 = $db->quote($MD5);
+					$query = "select Target from Links where Source=$MD5;";
+					$sth = $db->prepare($query);
+					$sth->execute();
+					while(@row = $sth->fetchrow_array()){
+						if(!($row[0] eq $voter)){
+							$temp{$row[0]} += $rating{$voter} * $converge;
+							if(!$running){
+								return;
+							}
 						}
 					}
 				}
+				foreach(@urls){
+					$rating{$_} = $temp{$_};   #copy back...
+				}
 			}
 			foreach(@urls){
-				$rating{$_} = $temp{$_};   #copy back...
+				$query = "update `Sources` set Rank=";
+				$query .= $rating{$_} . " where URL=";
+				$query .= $db->quote($_) . ";";
+				$db->do($query);
 			}
-			#print "[DEBUG::Ranker] ", ($i + 1), "/10th done!\n" unless !$debug;
-		}
+        }
+		sleep 10;
+        $command = "";
 
-		foreach(@urls){
-			#  print $_, "\t", $rating{$_}, "\n";
-			$query = "update `Sources` set Rank=";
-			$query .= $rating{$_} . " where URL=";
-			$query .= $db->quote($_) . ";";
-			$db->do($query);
-		}
-		#print "[DEBUG::Ranker] $elements completed in " . (((time() - $start)/60)/60) . " hours\n" unless !$debug;
-		sleep 60;
-	}
-	$sth->finish;
-	$db->disconnect();
 }
-Ranker();
+close FIFO;

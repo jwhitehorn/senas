b7a7772c359b92226d8633ab6813e4e800d04031
Jason Whitehorn <jason@whitehorn.us>
2005-09-02 02:02:09 +0000

    made oracle into a stand alone daemon

--- oracle.pl
+++ oracle.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #The oracle copyright 2004, 2005, Jason Whitehorn
-my $version = "0.8.1a";  
+my $version = "0.8.2";  
 #This program is free software; you can redistribute it and/or
 #modify it under the terms of the GNU General Public License
 #as published by the Free Software Foundation; either version 2
@@ -14,14 +14,34 @@ my $version = "0.8.1a";
 #You should have received a copy of the GNU General Public License
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+use POSIX qw(setsid);
+use Fcntl;
+use DBI;    #only works with transactional MySQL
+use Digest::MD5 qw(md5_hex);
+use URI;	#for link absolution
 
+my $pipe = "/home/jason/foo.bar";
 my $config_file = "senas.cfg";
 my @parsers = ("./mime_parsers/html.pl");	#put your MIME-type parsers here!
+my $action_fail = 1;	#const
+my $action_update = 0;	#const
 
-my $DBPassword;# = "password";
-my $DBHost;# = "127.0.0.1";
-my $DB;# = "search";
-my $DBUser;# = "username";
+my $DBPassword;
+my $DBHost;
+my $DB;
+my $DBUser;
+my $revisit_in = (30 * 24 * 60 * 60);   #30 days....DUN DUN DUNNN!!!!
+
+sub load_handler{
+        my $filename = $_[0];
+        my $data;
+        open FILE, "<$filename";
+		while(<FILE>){
+                $data = $data . $_;
+        }
+        close FILE;
+        eval($data);
+}
 
 open FILE, "<$config_file";
 while(<FILE>){
@@ -40,267 +60,142 @@ while(<FILE>){
 }
 close FILE;
 
-use DBI;    #only works with transactional MySQL
-use Digest::MD5 qw(md5_hex);
-use URI;	#for link absolution
-#use threads;
-#use threads::shared;
-my $debug = 1;		#output debug messages to console, and runs inputReader
-my $run_ranker = 1;	#run page ranker thread
 
-
-my $action_fail = 1;
-my $action_update = 0;
-
-
-
-my $revisit_in = (30 * 24 * 60 * 60);   #30 days....DUN DUN DUNNN!!!!
-$running = 1;
-#share($running);
-#share($debug);
-
-sub load_handler{
-        my $filename = $_[0];
-        my $data;
-        open FILE, "<$filename";
-		while(<FILE>){
-                $data = $data . $_;
-        }
-        close FILE;
-        eval($data);
+if(lc($ARGV[0]) eq "stop"){
+        open FIFO, ">$pipe";
+        print FIFO "stop\n";
+        close FIFO;
+        exit 0;
 }
-sub inputReader{
-    #if child
-    while(1){
-        $input = <STDIN>;
-        $input =~ s/\n//g;
-        if("$input" eq "stop"){
-            print "Stopping...";
-            $running = 0;
-            return 0;
-        }
-    }
+if(!(lc($ARGV[0]) eq "start")){
+        print "Error, invalid argument!\n";
+        exit 1;
 }
-sub Ranker{
-	print "[DEBUG::Ranker] Ranker started.\n" unless !$debug;
-	$db = DBI->connect("DBI:mysql:$DB:$DBHost", "$DBUser", "$DBPassword")
-		or die "Error connecting to database\n";
-	while($running){
-		my $start = time();
-		my %rating = ();
-		my %temp = (); 
-		my %MD5s = ();
-		my @urls = ();
-		my $start = 0;
-		my $elements = 0;
-		my $converge = 0.001;
-		$query = "select URL, MD5 from `Sources`;";    #get EVERYTHING...this will take a while
-		$sth = $db->prepare($query);
-		$sth->execute();
-		$elements = $sth->rows;
-		my $URL;
-		print "[DEBUG::Ranker] Beginning ranking $elements elements.\n" unless !$debug;
-		while($rows = $sth->fetchrow_arrayref()){
-			$URL = $rows->[0];
-			$MD5 = $rows->[1];
-			$rating{$URL} = 1.0;
-			$MD5s{$URL} = $MD5;
-			push @urls, $URL;
-		}
-		for($i = 0; $i != 10; $i++){
-		# print "$i";
-		#	print ".";
-			foreach (@urls){
-				$temp{$_} = $rating{$_};   #make a copy!
-			}
-			foreach (@urls){
-			#    print "|";
-				my $voter = $_;
-				$MD5 = $MD5s{$voter};
-				$MD5 = $db->quote($MD5);
-				$query = "select Target from Links where Source=$MD5;";
-				$sth = $db->prepare($query);
-				$sth->execute();
-				while(@row = $sth->fetchrow_array()){
-					if(!($row[0] eq $voter)){
-						$temp{$row[0]} += $rating{$voter} * $converge;
-						if(!$running){
-							return;
+#else... start the daemon
+
+chdir("/");
+open STDIN, '/dev/null';
+open STDOUT, '>/dev/null';
+open STDERR, '>/dev/null';
+umask(0);
+my $pid = fork();
+exit if $pid;   #exit if we are the parent
+setsid or die $!;
+sysopen(FIFO, "$pipe", O_NONBLOCK|O_RDONLY) or die $!;
+
+my $debug = 0;
+my $command;
+my $db = DBI->connect("DBI:mysql:$DB:$DBHost", "$DBUser", "$DBPassword") or die "Error connection!\n";
+while(1){
+        $command = <FIFO>;
+        if($command =~ m/stop/i){
+				$sth->finish();
+				$db->disconnect();
+                exit;   #got stop command!
+        }else{
+			$db->do("begin;");  #start transaction
+			$query = "select URL, Data, LastSeen, Action, Type from incoming order by LastSeen asc limit 1";
+			$sth = $db->prepare($query);
+			$sth->execute();
+			if($sth->rows > 0){	#if the oracle has something to do!!!!
+				print "[DEBUG::oracle] Something to do!\n" unless !$debug;
+				$rows = $sth->fetchrow_arrayref();
+				my $url = $rows->[0];
+				my $data = $rows->[1];
+				my $LastSeen = $rows->[2];
+				my $action = $rows->[3];
+				my $type = $rows->[4];
+				my $MD5 = md5_hex($data);
+				if($action == $action_update){
+					print "[DEBUG::oracle] update request for $MD5\n" unless !$debug;
+					#We have a valid URL coming in....
+					$query = "select MD5 from `Index` where MD5=";
+					$query .= $db->quote($MD5) . ";";
+					$sth = $db->prepare($query);
+					$sth->execute();
+					if($sth->rows == 0){ #if this is the first time we have seen this exact data
+						print "[DEBUG::oracle] item is new entry\n" unless !$debug;
+						$lnk = $db->quote($url);
+						$s = $db->prepare("select MD5 from `Sources` where URL=$lnk;");
+						$s->execute();
+						if($s->rows > 0){	#we have seen this URL before, and it has changed.			
+							$dup = $s->fetchrow_arrayref();
+							$chk = $db->quote($dup->[0]);
+							print "[DEBUG::oracle] Dup. entry, deleting MD5=", $dup->[0], "\n" unless !$debug;
+							$db->do("delete from `Sources` where URL=$lnk;");	#remove this old entry
+							$s = $db->prepare("select MD5 from `Sources` where MD5=$chk;");
+							$s->execute();
+							if($s->rows == 0){	#if this was the only source we just deleted...then
+								$db->do("delete from `Index` where MD5=$chk;");
+								$db->do("delete from Links where Source=$chk;");
+								$db->do("delete from WordIndex where MD5=$chk;");
+							}
 						}
+						#insert into Index
+						$query = "insert into `Index` (MD5, Cache, TSize) values (";
+						$query .= $db->quote($MD5) . ", " . $db->quote($data) . ", " . length($data) . ");";
+						$sth = $db->prepare($query);
+						$sth->execute();	#insert into index....
+
+						#find a parser for the particular MIME type
+						$found_handler = 0;	#we have not found a handler yet, so don't assume anything
+						foreach $handler (@parsers){
+							print "[DEBUG::oracle] Loading handler ", $handler, "\n" unless !$debug;
+							load_handler($handler);
+							if(handler_type() == $type){
+								$found_handler = 1;
+								handler($db, $data, $url, $MD5);
+							}
+						}
+						if($found_handler == 0){
+								print "[DEBUG::oracle] -->> No parser found for MIME type: $type\n" unless !$debug;
+						}
+						#obviously we do not know of this source....so put it in sources
+						$query = "insert into Sources (URL, MD5, LastSeen, Type) values (";
+						$query .= $db->quote($url) . ", " . $db->quote($MD5) . ", $LastSeen, ";
+						$query .= $db->quote($type) . ");";
+						$db->do($query);	#insert into sources
+					}else{   #we have seen this data before
+						$query = "select MD5 from Sources where MD5=";
+						$query .= $db->quote($MD5) . ";";
+						$sth = $db->prepare($query);
+						$sth->execute();
+						if($sth->rows == 0){
+							#IF this is the first time to seen this source...add it to Sources
+							$query = "insert into Sources (URL, MD5, LastSeen, Type) values (";
+							$query .= $db->quote($url) . ", " . $db->quote($MD5) . ", $LastSeen, ";
+							$query .= $db->quote($type) . ");";
+							$db->do($query);
+						}else{
+							#ELSE update the LastSeen value for this source
+							$query = "update `Sources` set LastSeen=$LastSeen where URL=";
+							$query .= $db->quote($url) . ";";
+							$db->do($query);
+							$query = "update `Sources` set Type=" . $db->quote($type);
+							$query .= " where URL=" . $db->quote($url) . ";";
+							$db->do($query);
+						}
+					}
+				}else{
+					if($action == $action_fail){
+						#something we asked for is not valid....
+						#delete it from the DB if it exists in the DB
 					}
 				}
+				#delete item from incoming...so we don't double our work ;-)
+				$db->do("delete from incoming where LastSeen=$LastSeen and Data=" . $db->quote($data) . ";");
+			}else{  #if there is nothing in the incoming table
+				print "[DEBUG::oracle] Nothing to do, sleeping\n" unless !$debug;
+				sleep 60 * 2; #we will sleep a little extra this time around...
+				
+				#insert into outgoing sources we have not seen in $revisit_in time!
 			}
-			foreach(@urls){
-				$rating{$_} = $temp{$_};   #copy back...
-			}
-			print "[DEBUG::Ranker] ", ($i + 1), "/10th done!\n" unless !$debug;
-		}
-
-		foreach(@urls){
-			#  print $_, "\t", $rating{$_}, "\n";
-			$query = "update `Sources` set Rank=";
-			$query .= $rating{$_} . " where URL=";
-			$query .= $db->quote($_) . ";";
-			$db->do($query);
-		}
-		print "[DEBUG::Ranker] $elements completed in " . (((time() - $start)/60)/60) . " hours\n" unless !$debug;
-	}
-	$sth->finish;
-	$db->disconnect();
-}
-if($debug){
-	print "Project Senas (http://www.senas.org), copyright 2004, 2005, Jason Whitehorn.\n";
-	print "oracle version $version\n";
-	print "\tTo shutdown, type 'stop'\n\n";
-}
-#$Ithread = threads->new(\&inputReader) or die "Error creating I thread.\n";
-#if( $run_ranker) {
-#	$Rthread = threads->new(\&Ranker) or die "Error creating Ranker thread.\n";
-#}
-
-sub indexer{
-    $db = DBI->connect("DBI:mysql:$DB:$DBHost", "$DBUser", "$DBPassword") or die "Error connection!\n";
-    while($running){   #run-time loop		
-        $db->do("begin;");  #start transaction
-        $query = "select URL, Data, LastSeen, Action, Type from incoming order by LastSeen asc limit 1";
-        $sth = $db->prepare($query);
-        $sth->execute();
-        if($sth->rows > 0){	#if the oracle has something to do!!!!
-            print "[DEBUG::oracle] Something to do!\n" unless !$debug;
-            $rows = $sth->fetchrow_arrayref();
-            my $url = $rows->[0];
-            my $data = $rows->[1];
-            my $LastSeen = $rows->[2];
-            my $action = $rows->[3];
-            my $type = $rows->[4];
-            my $MD5 = md5_hex($data);
-            if($action == $action_update){
-                print "[DEBUG::oracle] update request for $MD5\n" unless !$debug;
-                #We have a valid URL coming in....
-                $query = "select MD5 from `Index` where MD5=";
-                $query .= $db->quote($MD5) . ";";
-                $sth = $db->prepare($query);
-                $sth->execute();
-                if($sth->rows == 0){ #if this is the first time we have seen this exact data
-                    print "[DEBUG::oracle] item is new entry\n" unless !$debug;
-					$lnk = $db->quote($url);
-					$s = $db->prepare("select MD5 from `Sources` where URL=$lnk;");
-					$s->execute();
-					if($s->rows > 0){	#we have seen this URL before, and it has changed.			
-						$dup = $s->fetchrow_arrayref();
-						$chk = $db->quote($dup->[0]);
-						print "[DEBUG::oracle] Dup. entry, deleting MD5=", $dup->[0], "\n" unless !$debug;
-						$db->do("delete from `Sources` where URL=$lnk;");	#remove this old entry
-						$s = $db->prepare("select MD5 from `Sources` where MD5=$chk;");
-						$s->execute();
-						if($s->rows == 0){	#if this was the only source we just deleted...then
-							#print "Index removed\n";
-							$db->do("delete from `Index` where MD5=$chk;");
-							$db->do("delete from Links where Source=$chk;");
-							$db->do("delete from WordIndex where MD5=$chk;");
-						}
-					}
-                    #insert into Index
-                    $query = "insert into `Index` (MD5, Cache, TSize) values (";
-                    $query .= $db->quote($MD5) . ", " . $db->quote($data) . ", " . length($data) . ");";
-                    $sth = $db->prepare($query);
-                    $sth->execute();	#insert into index....
-
-					#find a parser for the particular MIME type
-					$found_handler = 0;	#we have not found a handler yet, so don't assume anything
-					foreach $handler (@parsers){
-						print "[DEBUG::oracle] Loading handler ", $handler, "\n" unless !$debug;
-						load_handler($handler);
-						if(handler_type() == $type){
-							$found_handler = 1;
-							handler($db, $data, $url, $MD5);
-						}
-					}
-                    if($found_handler == 0){
-							print "[DEBUG::oracle] -->> No parser found for MIME type: $type\n" unless !$debug;
-					}
-                    #obviously we do not know of this source....so put it in sources
-					$query = "insert into Sources (URL, MD5, LastSeen, Type) values (";
-                    $query .= $db->quote($url) . ", " . $db->quote($MD5) . ", $LastSeen, ";
-                    $query .= $db->quote($type) . ");";
-					$db->do($query);	#insert into sources
-                }else{   #we have seen this data before
-                    $query = "select MD5 from Sources where MD5=";
-                    $query .= $db->quote($MD5) . ";";
-                    $sth = $db->prepare($query);
-                    $sth->execute();
-                    if($sth->rows == 0){
-                        #IF this is the first time to seen this source...add it to Sources
-                        $query = "insert into Sources (URL, MD5, LastSeen, Type) values (";
-                        $query .= $db->quote($url) . ", " . $db->quote($MD5) . ", $LastSeen, ";
-                        $query .= $db->quote($type) . ");";
-                        $db->do($query);
-                    }else{
-                        #ELSE update the LastSeen value for this source
-                        $query = "update `Sources` set LastSeen=$LastSeen where URL=";
-                        $query .= $db->quote($url) . ";";
-                        $db->do($query);
-                        $query = "update `Sources` set Type=" . $db->quote($type);
-                        $query .= " where URL=" . $db->quote($url) . ";";
-                        $db->do($query);
-                    }
-                }
-            }else{
-                if($action == $action_fail){
-                    #something we asked for is not valid....
-                    #delete it from the DB if it exists in the DB
-                }
-            }
-            #delete item from incoming...so we don't double our work ;-)
-            $db->do("delete from incoming where LastSeen=$LastSeen and Data=" . $db->quote($data) . ";");
-        }else{  #if there is nothing in the incoming table
-            print "[DEBUG::oracle] Nothing to do, sleeping\n" unless !$debug;
-            sleep 60 * 2; #we will sleep a little extra this time around...
-			
-			#insert into outgoing sources we have not seen in $revisit_in time!
+			$db->do("commit;");
+			$query = "delete from `QueryCache` where `Expire` < " . time() . ";"; 
+			$db->do($query);    #delete outdated query cache entries
         }
-        $db->do("commit;");
-        $query = "delete from `QueryCache` where `Expire` < " . time() . ";"; 
-        $db->do($query);    #delete outdated query cache entries
-    }#end run-time loop
-    $sth->finish();
-    $db->disconnect();
-    $Ithread->join();
-	$Rthread->join();
-    print "DONE\n";
-    exit(1);
-}
+        $command = "";
 
-#main program
-my $pid = fork();
-if(!defined($pid)){
-    print "Error spawning child!\n";
-    exit $!;
 }
-if($pid == 0){  #child process
-    #$stop = 0;
-    #my $thread = threads->new(\&index) or die "Error spawning index thread.\n";
-	indexer();
-    #while(1){
-    #    sleep 60;
-    #}
-}
-my $index_pid = $pid;	#save
-$pid = fork();
-if(!defined($pid)){
-    print "Error spawning child!\n";
-    exit $!;
-}
-if($pid == 0){  #child process
-   if($run_ranker){
-        Ranker();
-	}
-}
-my $ranker_pid = $pid;
-if($debug){
-	inputReader();
-	#sleep 360;
-	#kill $index_pid;
-	#kill $ranker_pid;
-}     
-exit 1; #parent return
+close FIFO;
+
